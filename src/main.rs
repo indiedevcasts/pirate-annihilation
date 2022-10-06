e384230dfb2ddc62dad1df7e5360ee93465f64f9
Auff <adrien.auffredou@gmail.com>
2022-10-06 23:27:16 +0200

    Add Ldtk

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(add_people)
-        .add_system(greet_people)
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(setup)
+        .insert_resource(LevelSelection::Index(0))
+        .register_ldtk_entity::<MyBundle>("LdtkEntity")
         .run();
 }
 
-#[derive(Component)]
-struct Person;
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // Hot reload https://bevy-cheatbook.github.io/assets/hot-reload.html
+    asset_server.watch_for_changes().unwrap();
 
-#[derive(Component)]
-struct Name(String);
+    commands.spawn_bundle(Camera2dBundle::default());
 
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Elaina Proctor".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Renzo Hume".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Zayna Nieves".to_string()));
+    commands.spawn_bundle(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("map.ldtk"),
+        ..Default::default()
+    });
+}
+#[derive(Component, Default)]
+struct Character {
+    pub name: String,
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
-    }
+#[derive(Bundle, LdtkEntity)]
+pub struct MyBundle {
+    pouet: Character,
+    #[sprite_sheet_bundle]
+    #[bundle]
+    sprite_bundle: SpriteSheetBundle,
 }
