10fbfd8a20a64c9191335e712412a2fb1c8c5f13
theredfish <did.julian@gmail.com>
2022-10-20 19:01:45 +0200

    Fix jittery movement
    Replace fix delta by the system delta time. Also refactor the
    movement direction for updating the translation.

--- src/systems/player_system.rs
+++ src/systems/player_system.rs
@@ -1,33 +1,36 @@
-use bevy::prelude::{Input, KeyCode, Query, Res, Transform, Vec3, With};
-
 use crate::components::player::Player;
+use bevy::prelude::*;
 
-const TIME_STEP: f32 = 1.0 / 60.0;
+const MOVEMENT_SPEED: f32 = 500.;
+const MAX_DEGREES: f32 = 360.;
+const ROTATION_STEP: f32 = 0.5;
+const FORWARD_STEP: f32 = 1.;
 
-pub fn movement(input: Res<Input<KeyCode>>, mut query: Query<&mut Transform, With<Player>>) {
+pub fn movement(
+    time: Res<Time>,
+    input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+) {
     for mut transform in query.iter_mut() {
-        let mut rotation_factor = 0.0;
-        let mut movement_factor = 0.0;
+        let mut rotation_input = 0.;
+        let mut forward_input = 0.;
+
         if input.pressed(KeyCode::Z) {
-            movement_factor -= 1.0;
+            forward_input = -FORWARD_STEP;
+
             if input.pressed(KeyCode::D) {
-                rotation_factor -= 0.5;
+                rotation_input -= ROTATION_STEP;
             }
             if input.pressed(KeyCode::Q) {
-                rotation_factor += 0.5;
+                rotation_input += ROTATION_STEP;
             }
         }
 
-        transform.rotate_z(rotation_factor * f32::to_radians(360.0) * TIME_STEP);
+        // Rotation
+        transform.rotate_z(rotation_input * f32::to_radians(MAX_DEGREES) * time.delta_seconds());
 
-        // get the ship's forward vector by applying the current rotation to the ships initial facing vector
-        let movement_direction = transform.rotation * Vec3::Y;
-
-        // get the distance the ship will move based on direction, the ship's movement speed and delta time
-        let movement_distance = movement_factor * 500.0 * TIME_STEP;
-
-        let translation_delta = movement_direction * movement_distance;
-
-        transform.translation += translation_delta;
+        // Movement
+        let direction = transform.rotation * Vec3::Y * forward_input;
+        transform.translation += direction * MOVEMENT_SPEED * time.delta_seconds();
     }
 }
