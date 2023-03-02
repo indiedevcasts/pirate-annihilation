f8030c2da1ce8fbc386cf7366b459a5440aa91f7
theredfish <did.julian@gmail.com>
2023-03-02 15:22:05 +0100

    Clean code before adding hex meshes

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,4 @@
-use crate::components::player::Player;
 use bevy::{input::mouse::MouseMotion, prelude::*};
-use bevy_ecs_ldtk::{LdtkLevel, LevelSelection};
 use dolly::prelude::*;
 
 const MOVEMENT_SPEED: f32 = 10.;
@@ -10,65 +8,6 @@ const VERTICAL_STEP: f32 = 1.;
 #[derive(Component, Debug)]
 pub struct CameraController {
     pub rig: CameraRig,
-}
-
-pub fn fit_inside_current_level(
-    mut camera_query: Query<
-        (
-            &mut bevy::render::camera::OrthographicProjection,
-            &mut Transform,
-        ),
-        Without<Player>,
-    >,
-    player_query: Query<&Transform, With<Player>>,
-    level_query: Query<
-        (&Transform, &Handle<LdtkLevel>),
-        (Without<OrthographicProjection>, Without<Player>),
-    >,
-    level_selection: Res<LevelSelection>,
-    ldtk_levels: Res<Assets<LdtkLevel>>,
-    time: Res<Time>,
-) {
-    if let Ok(Transform {
-        translation: player_translation,
-        ..
-    }) = player_query.get_single()
-    {
-        let player_translation = *player_translation;
-        let (orthographic_projection, mut camera_transform) = camera_query.single_mut();
-        for (_, level_handle) in level_query.iter() {
-            if let Some(ldtk_level) = ldtk_levels.get(level_handle) {
-                // The boundaries used to clamp the camera in the level
-                let (x_boundary_distance, y_boundary_distance) = (
-                    orthographic_projection.right * orthographic_projection.scale,
-                    orthographic_projection.top * orthographic_projection.scale,
-                );
-                let level = &ldtk_level.level;
-                if level_selection.is_match(&0, level) {
-                    let x = (player_translation.x).clamp(
-                        x_boundary_distance,
-                        level.px_wid as f32 - x_boundary_distance,
-                    );
-                    let y = (player_translation.y).clamp(
-                        y_boundary_distance,
-                        level.px_hei as f32 - y_boundary_distance,
-                    );
-
-                    let direction = Vec3::new(x, y, camera_transform.translation.z);
-
-                    let smooth_damp = magnet::core::vec3::smooth_damp(
-                        camera_transform.translation,
-                        direction,
-                        Vec3::ZERO,
-                        0.2,
-                        f32::INFINITY,
-                        time.delta_seconds(),
-                    );
-                    camera_transform.translation = smooth_damp;
-                }
-            }
-        }
-    }
 }
 
 pub fn move_free_camera(
@@ -81,12 +20,12 @@ pub fn move_free_camera(
 ) {
     let (mut camera_transform, _) = match camera.get_single_mut() {
         Ok(transform) => transform,
-        _ => return (),
+        _ => return,
     };
 
     let mut camera_controller = match camera_controller.get_single_mut() {
         Ok(controller) => controller,
-        _ => return (),
+        _ => return,
     };
 
     let mut horizontal_move = 0.;
@@ -111,8 +50,6 @@ pub fn move_free_camera(
     let move_vec = camera_controller.rig.final_transform.rotation
         * Vec3::new(horizontal_move, 0., vertical_move).clamp_length_max(1.0)
         * MOVEMENT_SPEED;
-
-    debug!("camera_transform : {:?}", camera_transform);
 
     // camera rotation
     if mouse_input.pressed(MouseButton::Right) {
--- src/hex/mod.rs
+++ src/hex/mod.rs
@@ -3,8 +3,7 @@
 //! Based on the excellent tutorial written by Jasper Flick.
 //! See https://catlikecoding.com/unity/tutorials/hex-map.
 
-use bevy::{prelude::*, render::render_resource::PrimitiveTopology};
-use std::sync::Mutex;
+use bevy::prelude::*;
 
 /// Hexagon's outer radius.
 const OUTER_RADIUS: f32 = 10.;
@@ -12,7 +11,7 @@ const OUTER_RADIUS: f32 = 10.;
 /// Hexagon's inner radius.
 /// The inner radius is equals to √3/2 times the outer radius.
 /// In our case it's equals to 5 * √3.
-const INNER_RADIUS: f32 = OUTER_RADIUS * 0.866025404;
+const INNER_RADIUS: f32 = OUTER_RADIUS * 0.866_025_4;
 
 /// Hexagon's corners.
 /// Orientation is pointy side up ⬡.
@@ -25,15 +24,40 @@ const CORNERS: [Vec3; 6] = [
     Vec3::new(-INNER_RADIUS, 0., 0.5 * OUTER_RADIUS),
 ];
 
-const PLANE_SIZE: f32 = 10.;
+#[derive(Clone)]
+pub struct HexCell {
+    pub position: Vec3,
+    pub color: Color,
+    pub pbr_bundle: PbrBundle,
+}
 
-pub struct HexCell;
+impl HexCell {
+    pub fn new(
+        position: Vec3,
+        color: Color,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        materials: &mut ResMut<Assets<StandardMaterial>>,
+    ) -> Self {
+        let pbr_bundle = PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: OUTER_RADIUS })),
+            material: materials.add(color.into()),
+            transform: Transform::from_translation(position),
+            ..default()
+        };
+
+        Self {
+            position,
+            color,
+            pbr_bundle,
+        }
+    }
+}
 
 #[derive(Default)]
 pub struct HexGrid {
     pub width: u8,
     pub height: u8,
-    pub cells: Vec<PbrBundle>,
+    pub cells: Vec<HexCell>,
 }
 
 impl HexGrid {
@@ -44,14 +68,16 @@ impl HexGrid {
             ..default()
         }
     }
-    pub fn generate(
+
+    pub fn render(
         &mut self,
+        commands: &mut Commands,
         mut meshes: ResMut<Assets<Mesh>>,
         mut materials: ResMut<Assets<StandardMaterial>>,
     ) {
         for z in 0..self.height {
             for x in 0..self.width {
-                let position = Vec3::new(x as f32 * PLANE_SIZE, 0., z as f32 * PLANE_SIZE);
+                let position = Vec3::new(x as f32 * OUTER_RADIUS, 0., z as f32 * OUTER_RADIUS);
                 let color = if x == 0 && z == 0 {
                     Color::ORANGE_RED
                 } else if x % 2 == 0 {
@@ -60,14 +86,10 @@ impl HexGrid {
                     Color::ORANGE
                 };
 
-                let plane = PbrBundle {
-                    mesh: meshes.add(Mesh::from(shape::Plane { size: PLANE_SIZE })),
-                    material: materials.add(color.into()),
-                    transform: Transform::from_translation(position),
-                    ..default()
-                };
+                let cell = HexCell::new(position, color, &mut meshes, &mut materials);
+                commands.spawn(cell.pbr_bundle.clone());
 
-                self.cells.push(plane)
+                self.cells.push(cell);
             }
         }
     }
